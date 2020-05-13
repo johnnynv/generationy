1ea9a49f5fed38f5bdc11539ac7a6f1e6df6381a
johnnynv <46965159+johnnynv@users.noreply.github.com>
2020-05-13 21:55:49 +0800

    Init r file for Natural Environment Perspective

---
+++ nep.r
@@ -0,0 +1,15 @@
+importcsv <- function(){
+	sdata <- read.csv("trans.csv")
+	return(sdata)
+}
+
+calc_mean_nep <- function(nep){
+	if(1=length(nep){
+		if("integer"=typeof(nep)){
+			nstr<-paste("NEP",nep,sep="")
+			return(mean(sdata[[nstr]]))
+		} else if
+	}
+}
+
+getwd()
